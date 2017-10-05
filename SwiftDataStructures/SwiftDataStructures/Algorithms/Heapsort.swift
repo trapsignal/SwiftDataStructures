a51460ad7b7ac20a9f7d9ffdf82105fee9df0b4b
trapsignal <trapsignal@yahoo.com>
2017-10-05 17:57:57 +0300

    heapsort refactoring

--- SwiftDataStructures/SwiftDataStructures/Algorithms/Heapsort.swift
+++ SwiftDataStructures/SwiftDataStructures/Algorithms/Heapsort.swift
@@ -5,6 +5,47 @@
 //  @author trapsignal <trapsignal@yahoo.com>
 //
 
+// MARK: - Heapsort
+
+extension MutableCollection where Element: Comparable {
+
+    public mutating
+    func heapsort(by areInIncreasingOrder: @escaping (Element, Element) throws -> Bool) rethrows {
+        let heap = BinaryHeap<Element>(self)
+        try heap.build(by: areInIncreasingOrder)
+        try heap.sort(by: areInIncreasingOrder)
+
+        rewriteWith(heap.items)
+    }
+
+    public
+    func heapsorted(
+        by areInIncreasingOrder: @escaping (Element, Element) throws -> Bool
+    ) rethrows -> Self {
+        var newCollection = self
+        try newCollection.heapsort(by: areInIncreasingOrder)
+        return newCollection
+    }
+
+}
+
+// MARK: - Implementation
+
+private
+extension MutableCollection where Element: Comparable {
+
+    mutating
+    func rewriteWith(_ items: [Element]) {
+        var index = startIndex
+        for i in 0 ..< items.count {
+            self[index] = items[i]
+            index = self.index(after: index)
+        }
+    }
+
+}
+
+private
 extension Collection {
 
     func element(at index: Index, within range: Range<Index>? = nil) -> Element? {
@@ -14,37 +55,71 @@ extension Collection {
 
 }
 
+// MARK: - BinaryHeap
+
+private
 class BinaryHeap<Value: Comparable> {
+
+    // MARK: Properties
 
     private(set)
     var items: [Value] = []
+
+    private
+    var defaultRange: Range<Int> {
+        return items.startIndex ..< items.endIndex
+    }
+
+    // MARK: Initialization
 
     init<S: Sequence>(_ items: S) where S.Iterator.Element == Value {
         self.items = Array(items)
     }
 
+    // MARK: File Level API
+
+    fileprivate
+    func build(by areInIncreasingOrder: (Value, Value) throws -> Bool) rethrows {
+        for i in (0 ..< items.count / 2).reversed() {
+            try heapify(by: areInIncreasingOrder, at: i)
+        }
+
+        try checkSelf(by: areInIncreasingOrder)
+    }
+
+    fileprivate
+    func sort(by areInIncreasingOrder: (Value, Value) throws -> Bool) rethrows {
+        for i in (0 ..< items.count).reversed() {
+            let range: Range<Int> = 0 ..< i + 1
+            try exchangeWithRoot(by: areInIncreasingOrder, at: i, range: range)
+        }
+    }
+
+    // MARK: Implementation
+
+    private
     func leftIndex(for index: Int) -> Int {
         return index * 2 + 1
     }
 
+    private
     func left(for index: Int, range: Range<Int>) -> Value? {
         let leftIndex = self.leftIndex(for: index)
         return items.element(at: leftIndex, within: range)
     }
 
+    private
     func rightIndex(for index: Int) -> Int {
         return index * 2 + 2
     }
 
+    private
     func right(for index: Int, range: Range<Int>) -> Value? {
         let rightIndex = self.rightIndex(for: index)
         return items.element(at: rightIndex, within: range)
     }
 
-    var defaultRange: Range<Int> {
-        return items.startIndex ..< items.endIndex
-    }
-
+    private
     func heapify(
         by areInIncreasingOrder: (Value, Value) throws -> Bool,
         at index: Int,
@@ -78,20 +153,14 @@ class BinaryHeap<Value: Comparable> {
         }
     }
 
-    func build(by areInIncreasingOrder: (Value, Value) throws -> Bool) rethrows {
-        for i in (0 ..< items.count / 2).reversed() {
-            try heapify(by: areInIncreasingOrder, at: i)
-        }
-
-        try checkSelf(by: areInIncreasingOrder)
-    }
-
+    private
     func checkSelf(by areInIncreasingOrder: (Value, Value) throws -> Bool) rethrows {
         if !items.isEmpty {
             try check(by: areInIncreasingOrder, at: 0)
         }
     }
 
+    private
     func check(by areInIncreasingOrder: (Value, Value) throws -> Bool, at index: Int) rethrows {
         guard let value = items.element(at: index) else {
             return assertionFailure()
@@ -110,6 +179,7 @@ class BinaryHeap<Value: Comparable> {
         }
     }
 
+    private
     func exchangeWithRoot(
         by areInIncreasingOrder: (Value, Value) throws -> Bool,
         at index: Int,
@@ -125,32 +195,6 @@ class BinaryHeap<Value: Comparable> {
         items[0] = value
         try heapify(by: areInIncreasingOrder, at: 0, range: range)
         items[index] = max
-    }
-
-    func sort(by areInIncreasingOrder: (Value, Value) throws -> Bool) rethrows {
-        for i in (0 ..< items.count).reversed() {
-            let range: Range<Int> = 0 ..< i + 1
-            try exchangeWithRoot(by: areInIncreasingOrder, at: i, range: range)
-        }
-    }
-
-}
-
-extension Array where Element: Comparable {
-
-    public mutating
-    func heapsort(by areInIncreasingOrder: @escaping (Element, Element) throws -> Bool) rethrows {
-        let heap = BinaryHeap<Element>(self)
-        try heap.build(by: areInIncreasingOrder)
-        try heap.sort(by: areInIncreasingOrder)
-        self = heap.items
-    }
-
-    public
-    func heapsorted(by areInIncreasingOrder: @escaping (Element, Element) throws -> Bool) rethrows -> Array {
-        var newArray = self
-        try newArray.heapsort(by: areInIncreasingOrder)
-        return newArray
     }
 
 }
